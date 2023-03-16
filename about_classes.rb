a11083e060893956e2bb03685302080c69c8cbf6
n1mets <99960812+n1mets@users.noreply.github.com>
2023-03-16 19:56:24 +0200

    Add files via upload

--- about_classes.rb
+++ about_classes.rb
@@ -159,12 +159,12 @@ class AboutClasses < Neo::Koan
     fido = Dog7.new("Fido")
 
     fidos_self = fido.get_self
-    assert_equal __, fidos_self
+    assert_equal fido, fidos_self
   end
 
   def test_to_s_provides_a_string_version_of_the_object
     fido = Dog7.new("Fido")
-    assert_equal __, fido.to_s
+    assert_equal "Fido", fido.to_s
   end
 
   def test_to_s_is_used_in_string_interpolation
